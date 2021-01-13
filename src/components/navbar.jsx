52bb9c2dd697c65ba7633fba2ea3a4ff37b35618
Beto Rendon <betorendonruiz@gmail.com>
2021-01-13 17:17:06 -0600

    Added fontawesome, navbar example and head

---
+++ src/components/navbar.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import navStyles from "../styles/components/navbar.module.scss";
+import { faTerminal } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+class Navbar extends Component {
+  state = {
+    scrolled: false,
+    open: true
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.navOnScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.navOnScroll)
+  }
+
+  navOnScroll = () => {
+    if (window.scrollY > 20) {
+      this.setState({ scrolled: true })
+    } else {
+      this.setState({ scrolled: false })
+    }
+  }
+  
+  handleClick = () => {
+    if (this.state.open === true) {
+      this.setState({ open: false })
+    } else {
+      this.setState({ open: true })
+    }
+  }
+  render() {
+    const { scrolled } = this.state
+    const { open } = this.state
+    return (
+      <nav className={scrolled ? navStyles.black : navStyles.transparent}>
+          <div className={navStyles.menuIcon}>
+            
+          <FontAwesomeIcon icon={faTerminal} transform="down-7 grow-2.5" onClick={this.handleClick}/>
+          </div>
+          <Link className={navStyles.logo} to="/">Esperanza al Debil</Link>
+          <ul className={open ? navStyles.showing : ""}>
+            <li>
+              <Link className={navStyles.listLink} to="/">Lugar1</Link>
+            </li>
+            <li>
+              <Link className={navStyles.listLink} to="/">Lugar2</Link>
+            </li>
+            <li>
+              <Link className={navStyles.listLink} to="/">Lugar3</Link>
+            </li>
+            <li>
+              <Link className={navStyles.listLink} to="/">Lugar4</Link>
+            </li>
+          </ul>
+      </nav>
+    )
+  }
+}
+export default Navbar;
