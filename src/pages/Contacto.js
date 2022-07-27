7f083b17b1a244fb5ccf1d610b47ab8692d6789b
Luis Leyva <a01570367@itesm.mx>
2022-07-27 16:07:28 -0500

    Icons Color Change

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "../components/layout";
 import Head from "../components/head";
 import { Card, Button, Image } from "react-bootstrap";
-import { Link } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faInstagramSquare,
@@ -23,16 +22,17 @@ function Contacto() {
                     N.L., México
                 </p>
 
-                <div class="row justify-content-center">
+                <div className="row justify-content-center">
                     <iframe
                         title="Ubicación en Google Maps"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3594.368805710248!2d-100.36691358497978!3d25.725314583653265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x866297b8123fb51b%3A0x2cab3c0a0dc0fece!2sComedor%20Infantil%20Mensajero%20de%20Paz!5e0!3m2!1ses-419!2smx!4v1611898702545!5m2!1ses-419!2smx"
                         width="600"
                         height="450"
-                        frameborder="0"
-                        allowfullscreen=""
-                        aria-hidden="false"></iframe>
-
+                        frameBorder="0"
+                        allowFullScreen=""
+                        aria-hidden="false"
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"></iframe>
                     <Card className="col-lg-9 col-xl-5 mx-lg-auto ml-xl-auto mr-xl-0 my-2 py-4">
                         <strong>Nuestro teléfono</strong>
                         <p>
@@ -41,40 +41,44 @@ function Contacto() {
 
                         <strong>Escríbenos por WhatsApp</strong>
                         <p>
-                            <Link
-                                to="https://wa.me/message/AQESZAPMHK7WJ1"
-                                target="_blank">
+                            <a
+                                href="https://wa.me/message/AQESZAPMHK7WJ1"
+                                target="_blank"
+                                rel="noreferrer">
                                 <Image
                                     width="64px"
                                     height="64px"
                                     style={{ padding: "7px" }}
                                     src={require("../images/contacto/whatsapp-logo-1.png")}
                                 />
-                            </Link>
-                            <Link
-                                to="https://wa.me/message/AQESZAPMHK7WJ1"
-                                target="_blank">
+                            </a>
+                            <a
+                                href="https://wa.me/message/AQESZAPMHK7WJ1"
+                                target="_blank"
+                                rel="noreferrer">
                                 https://wa.me/message/AQESZAPMHK7WJ1
-                            </Link>
+                            </a>
                         </p>
 
                         <strong>Contáctanos por Messenger</strong>
                         <p>
-                            <Link
-                                to="http://m.me/esperanzaaldebil"
-                                target="_blank">
+                            <a
+                                href="http://m.me/esperanzaaldebil"
+                                target="_blank"
+                                rel="noreferrer">
                                 <Image
                                     width="64px"
                                     height="64px"
                                     style={{ padding: "3px" }}
                                     src={require("../images/contacto/messenger-logo-1024.png")}
                                 />
-                            </Link>
-                            <Link
-                                to="http://m.me/esperanzaaldebil"
-                                target="_blank">
+                            </a>
+                            <a
+                                href="http://m.me/esperanzaaldebil"
+                                target="_blank"
+                                rel="noreferrer">
                                 https://m.me/esperanzaaldebil
-                            </Link>
+                            </a>
                         </p>
 
                         <strong>Nuestro correo</strong>
@@ -89,26 +93,28 @@ function Contacto() {
                                 href="mailto:info@comedorinfantilmdepaz.org">
                                 Envíanos un correo
                             </Button>
-                            <Link
-                                to="https://www.facebook.com/comedormensajerodepaz"
-                                target="_blank">
+                            <a
+                                href="https://www.facebook.com/comedormensajerodepaz"
+                                target="_blank"
+                                rel="noreferrer">
                                 <FontAwesomeIcon
                                     icon={faFacebookSquare}
                                     size="3x"
                                     transform="down-5"
                                     className={contactStyles.socialIcon}
                                 />
-                            </Link>
-                            <Link
-                                to="https://www.instagram.com/comedorinfantilmensajero/"
-                                target="_blank">
+                            </a>
+                            <a
+                                href="https://www.instagram.com/comedorinfantilmensajero/"
+                                target="_blank"
+                                rel="noreferrer">
                                 <FontAwesomeIcon
                                     icon={faInstagramSquare}
                                     size="3x"
                                     transform="down-5"
                                     className={contactStyles.socialIcon}
                                 />
-                            </Link>
+                            </a>
                         </div>
                     </Card>
                 </div>
