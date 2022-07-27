20a1676068f74942c0686c2ad52449b1b9128aab
Luis Leyva <a01570367@itesm.mx>
2022-07-27 02:35:45 -0500

    Footer Spacing Fix

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Navbar from './navbar';
-import Footer from './Footer'
-import '../styles/global.module.scss';
+import React from "react";
+import Navbar from "./navbar";
+import Footer from "./Footer";
+import "../styles/global.module.scss";
 
 const Layout = (props) => {
     return (
         <div>
             <Navbar />
-            <div className="pt-5">
+            <div className="pt-5" style={{ minHeight: "94vh" }}>
                 {props.children}
             </div>
             <Footer />
         </div>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
