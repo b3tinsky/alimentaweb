1154516c28f6ab61f6034d971b2be331652808d2
Luis Leyva <a01570367@itesm.mx>
2022-07-29 18:42:18 -0500

    Comedores

--- src/pages/Comedores.js
+++ src/pages/Comedores.js
@@ -2,6 +2,7 @@ import React from "react";
 import ComedoresCard from "../components/ComedoresCard";
 import Layout from "../components/layout";
 import Head from "../components/head";
+import styles from "../styles/pages/Comedores.module.scss";
 
 function Comedores() {
     /**
@@ -13,38 +14,35 @@ function Comedores() {
             title: "Col. Carmen Serdán",
             description:
                 "Ofrecemos apoyo alimentario a niños, madres de familia y abuelitos, con el fin de complementar su plan nutricional familiar. También ofrecemos talleres formativos y de fomento educativo por proyecto. En este comedor se tiene un programa especial de atención a la primera infancia.",
-            images: ["comedores/1c1", "comedores/1c1", "comedores/1c1"],
+            images: ["comedores/1c1"],
         },
         {
             id: 1,
             title: "Col. Santa Cecilia",
             description:
                 "Ofrecemos talleres formativos, culturales, de fomento educativo y se otorgan alimentos nutritivos esenciales, especialmente para niños. Este comedor, tiene el fin de reducir a los niños en situación de calle y aumentar sus conocimientos y valores morales, para un desarrollo integral en la niñez.",
-            images: ["comedores/2c1", "comedores/2c1", "comedores/2c1"],
+            images: ["comedores/2c1"],
         },
         {
             id: 2,
             title: "Col. Garza Nieto (Apoyo alimentario)",
             description:
                 "Ofrecemos apoyo escolar a niños con algún tipo de rezago, a su vez, otorgamos apoyo en su alimentación básica para niños, madres de familia, abuelitos y a algunos migrantes de la zona.",
-            images: ["comedores/3c1", "comedores/3c1", "comedores/3c1"],
-
+            images: ["comedores/3c1"],
         },
         {
             id: 3,
-            title: "Juárez Nuevo León", 
+            title: "Juárez Nuevo León",
             description:
                 "Ofrecemos apoyo alimentario a niños, madres de familia y abuelitos, con el fin de complementar su plan nutricional familiar. También ofrecemos talleres formativos y de fomento educativo por proyecto. En este comedor se tiene un programa especial de atención a la primera infancia.",
-            images: ["comedores/3c1", "comedores/3c1", "comedores/3c1"], // actualizar imagenes
-
+            images: ["comedores/3c1"], // actualizar imagenes
         },
         {
             id: 4,
             title: "Cienega de Flores (Apoyo alimentario)",
             description:
                 "Ofrecemos apoyo escolar a niños con algún tipo de rezago, a su vez, otorgamos apoyo en su alimentación básica para niños, madres de familia, abuelitos y a algunos migrantes de la zona.",
-            images: ["comedores/3c1", "comedores/3c1", "comedores/3c1"],
-
+            images: ["comedores/3c1"], // actualizar imagenes
         },
     ];
 
@@ -57,20 +55,22 @@ function Comedores() {
             <Head title="Comedores" />
             <div className="container my-5">
                 <h1 className="display-4">Comedores</h1>
-                {/* <p className="lead">Optional description for this section</p> */}
-                <div class="row justify-content-center">
+                <p className="lead">
+                    Contamos con 5 ubicaciones en el area metropolitana de
+                    Monterrey y sus al rededores. En estas ubicaciones se
+                    proporcionan diferentes tipos de apoyo alimentario y
+                    formativos.
+                </p>
+                <div className={styles.cardContainer}>
                     {comedores.map((comedor) => (
                         <ComedoresCard
+                            key={`comedor-${comedor.id}`}
                             id={comedor.id}
                             title={comedor.title}
                             description={comedor.description}
                             images={comedor.images}
                         />
                     ))}
-                    <img
-                        src={require("../images/comedores/0c0.png")}
-                        class="img-fluid"
-                        alt="Ellos te necesitan"></img>
                 </div>
             </div>
         </Layout>
